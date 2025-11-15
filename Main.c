68e44bfd9acdade937fa0ae76dfbe131e1eb3605
Kamila Rivera M. <kamilarriveramunoz@gmail.com>
2025-11-14 23:39:36 -0500

    prueba

--- Main.c
+++ Main.c
@@ -7,18 +7,21 @@ int main()
 {
   int intentos;
   char user[6];
+  char opcion;
     do{
       printf("Ingrese usuario: \n")
       scanf("%s", &user);
       if(user.usuario!=txt.usuario){
         printf("Datos no coincidentes, intente nuevamente\n");
         intento++; 
-        
       }
-     
     }
     while(intentos<3);
-    exit(0);
+    printf("Sesion cerrada presione X para cerrar: \n");
+    scanf("%s", &pcion);
+    if(opcion=="X"){
+      exit(0);
+    }
     
     
     
